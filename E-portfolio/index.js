998eceb699db7a1ab23eace5e51d9b1e33f13d2e
Gabry701 <murgia.gabry2006@gmail.com>
2024-06-02 23:55:16 +0200

    i continued the website

---
+++ E-portfolio/index.js
@@ -0,0 +1,10 @@
+// // if (document.getElementsByClassName('more').)
+    
+    
+// // else 
+// //     setProperty('--displayMore', 'none');
+// var r = document.querySelector(':root');
+// function displayMore() {
+
+//         r.style.setProperty('--displayMore', 'block');
+//     }
